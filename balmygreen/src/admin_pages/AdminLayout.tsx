ac9fa6a8961e53565590e1464340cd3bf9da5ae7
bckim9489 <bckim9489@gmail.com>
2024-03-06 22:01:05 +0900

    설정

--- balmygreen/src/admin_pages/AdminLayout.tsx
+++ balmygreen/src/admin_pages/AdminLayout.tsx
@@ -1,11 +1,13 @@
 import {useState} from 'react';
-import { useTheme } from '@emotion/react';
+// import { useTheme } from '@emotion/react';
 import ButtonAppBar from '../components/ButtonAppBar';
 import ButtonDrawer from '../components/ButtonDrawer';
 
 export default function Layout({children}:any) {
+
+
   const [open, setOpen] = useState(false);
-  const theme = useTheme();
+  // const theme = useTheme();
 
   const toggleDrawer = (newOpen: boolean) => () => {
     setOpen(newOpen);
